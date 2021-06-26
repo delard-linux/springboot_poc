214a612f6e56671e5c8680066931f9014a0d46bc
David de la Rosa Delgado <drd_id@hotmail.com>
2021-06-26 11:59:44 +0200

    22. Obtener varios parámetros de la ruta con @PathVariable

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploVariablesRutaController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploVariablesRutaController.java
@@ -1,25 +1,41 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/variables")
 public class EjemploVariablesRutaController {
 
+	@GetMapping(value = {"/index", "/", ""})
+	public String params(Model model) {
+		model.addAttribute("titulo", "Enviar Parametros de la ruta (@PathVariable)");
+		return "variables/index";
+		
+	}
+	
 	
 	@GetMapping("/string/{texto}")
-	public String variable(@PathVariable(name="texto") String texto, 
+	public String variables(@PathVariable(name="texto") String texto, 
 						Model model) {
 
 		model.addAttribute("titulo", "Recibir Parametros de la ruta (@PathVariable)");
 		model.addAttribute("resultado", "El texto enviado en la ruta es: "+ texto);
+
+		return "variables/ver";
+		
+	}
+
+	@GetMapping("/string/{texto}/{numero}")
+	public String variables(@PathVariable(name="texto") String texto, 
+						@PathVariable(name="numero") Integer numero,
+						Model model) {
+
+		model.addAttribute("titulo", "Recibir Parametros de la ruta (@PathVariable)");
+		model.addAttribute("resultado", "El texto enviado en la ruta es '"+ texto + "' y el numero enviado en el path es '" + numero + "'");
 
 		return "variables/ver";
 		
