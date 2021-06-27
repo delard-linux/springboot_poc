7084642ccea482c0f16fdf328efafab1df05f3ce
David de la Rosa Delgado <drd_id@hotmail.com>
2021-06-27 10:34:21 +0200

    32. Implementando inyección de dependencia con la anotación @Autowired

--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controller/IndexController.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.di.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,8 @@ import com.bolsadeideas.springboot.di.app.models.services.MiServicio;
 @Controller
 public class IndexController {
 	
-	private MiServicio servicio =  new MiServicio();
+	@Autowired
+	private MiServicio servicio;
 	
 	@GetMapping({"/","","/index"})
 	public String index(Model model) {
--- spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/services/MiServicio.java
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/services/MiServicio.java
@@ -1,9 +1,12 @@
 package com.bolsadeideas.springboot.di.app.models.services;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MiServicio {
 	
 	public String operacion() {
-		return "### Esto es una operacion a traves de un servicio... ###";
+		return "### Esto es una operacion a traves de un servicio injectado... ###";
 	}
 
 }
