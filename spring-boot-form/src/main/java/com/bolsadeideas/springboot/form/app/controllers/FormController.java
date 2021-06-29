c9fe91592f5393fa6f9f1f5746dfc9bf9bb1c9e0
David de la Rosa Delgado <drd_id@hotmail.com>
2021-06-30 00:03:34 +0200

    50. Creando la clase model del formulario

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,9 +1,12 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class FormController {
 	
 	@GetMapping("/form")
@@ -12,7 +15,15 @@ public class FormController {
 	}
 
 	@PostMapping("/form")
-	public String procesar(Model model) {
+	public String procesar(Model model,
+			@RequestParam(name="username") String username,
+			@RequestParam(name="password") String password,
+			@RequestParam(name="email") String email
+			) {
+		model.addAttribute("titulo","Resultado del envío del formulario");
+		model.addAttribute("username",username);
+		model.addAttribute("password",password);
+		model.addAttribute("email",email);
 		return "resultado";
 	}
 
