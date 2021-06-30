613d260dc3bf271be1075c0afacc413ae10611fb
David de la Rosa Delgado <drd_id@hotmail.com>
2021-06-30 23:40:17 +0200

    53. Añadiendo los mensajes de errores en la vista del formulario con @ModelAttribute("user")

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
@@ -20,13 +21,13 @@ public class FormController {
 	public String form(Model model) {
 
 		model.addAttribute("titulo","Formulario de usuarios");
-		model.addAttribute("usuario",new Usuario());
+		model.addAttribute("user",new Usuario());
 
 		return "form";
 	}
 
 	@PostMapping("/form")
-	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+	public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model) {
 		
 		if(result.hasErrors()) {
 			Map<String, String> errores = new HashMap<>();
