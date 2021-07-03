824aed31a76ddb5d0eb301351735fc8343c0c725
David de la Rosa Delgado <drd_id@hotmail.com>
2021-07-03 23:28:16 +0200

    54. El atributo object de thymeleaf en el elemento form

--- spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ spring-boot-form/src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,15 +1,11 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
@@ -21,20 +17,15 @@ public class FormController {
 	public String form(Model model) {
 
 		model.addAttribute("titulo","Formulario de usuarios");
-		model.addAttribute("user",new Usuario());
+		model.addAttribute("usuario",new Usuario());
 
 		return "form";
 	}
 
 	@PostMapping("/form")
-	public String procesar(@Valid @ModelAttribute("user") Usuario usuario, BindingResult result, Model model) {
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
 		
 		if(result.hasErrors()) {
-			Map<String, String> errores = new HashMap<>();
-			result.getFieldErrors().forEach(err-> {
-				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
-			});
-			model.addAttribute("error",errores);
 			return "form";
 		}
 		
