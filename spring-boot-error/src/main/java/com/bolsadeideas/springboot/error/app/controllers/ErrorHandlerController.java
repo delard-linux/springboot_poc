cea983919edde8718cdb80b797e4e427ccb696e5
David de la Rosa Delgado <drd_id@hotmail.com>
2021-07-07 20:49:09 +0200

    105. Anotación @ControllerAdvice para manejo de excepciones

---
+++ spring-boot-error/src/main/java/com/bolsadeideas/springboot/error/app/controllers/ErrorHandlerController.java
@@ -0,0 +1,22 @@
+package com.bolsadeideas.springboot.error.app.controllers;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ErrorHandlerController {
+	
+	@ExceptionHandler(ArithmeticException.class)
+	public String aritmeticaError(ArithmeticException ex, Model model) {
+		model.addAttribute("error", "Error de aritmetica");
+		model.addAttribute("message", ex.getMessage());
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+		model.addAttribute("timestamp", new Date());
+		return "/error/aritmetica";
+	}
+
+}
