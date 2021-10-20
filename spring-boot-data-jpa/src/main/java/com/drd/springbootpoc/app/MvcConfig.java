122b68936070f28515e554b1ba0bba5db07c8ea0
David de la Rosa Delgado <drd_id@hotmail.com>
2021-10-20 06:49:42 +0200

    214.215 Configuración multilenguaje, Locale, Interceptores. Archivos de idiomas (es_ES, en_US)

--- spring-boot-data-jpa/src/main/java/com/drd/springbootpoc/app/MvcConfig.java
+++ spring-boot-data-jpa/src/main/java/com/drd/springbootpoc/app/MvcConfig.java
@@ -1,9 +1,15 @@
 package com.drd.springbootpoc.app;
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -13,6 +19,26 @@ public class MvcConfig implements WebMvcConfigurer {
 		return new BCryptPasswordEncoder();
 	}
 	
+	@Bean
+	public LocaleResolver localeResolver() {
+		var localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(new Locale("es","ES"));
+		return localeResolver;
+	}
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		var localeInterceptor = new LocaleChangeInterceptor();
+		localeInterceptor.setParamName("lang");
+		return localeInterceptor;
+		
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/error_403").setViewName("error_403");
