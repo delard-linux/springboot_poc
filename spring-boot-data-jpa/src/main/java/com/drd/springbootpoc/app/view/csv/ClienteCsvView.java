0e5702607a1a2e9f5c3aaae2b493368247da843a
David de la Rosa Delgado <drd_id@hotmail.com>
2021-12-25 18:14:05 +0100

    238. 241 View Technologies CSV  - net.sf.supercsv Additionally the version of apache poi changes to  5.1.0

---
+++ spring-boot-data-jpa/src/main/java/com/drd/springbootpoc/app/view/csv/ClienteCsvView.java
@@ -0,0 +1,57 @@
+package com.drd.springbootpoc.app.view.csv;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.view.AbstractView;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+
+import com.drd.springbootpoc.app.controllers.ConstantesController;
+import com.drd.springbootpoc.app.model.domain.ClienteDTO;
+
+@Component("listar")
+public class ClienteCsvView extends AbstractView {
+	
+	public ClienteCsvView() {
+		setContentType("text/csv");
+	}
+
+	@Override
+	protected boolean generatesDownloadContent() {
+		return true;
+	}
+	
+	@Override
+	protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		
+		response.setHeader("Content-Disposition", "attachment; filename=\"clientes.csv\"");
+		response.setContentType(getContentType());
+		
+		@SuppressWarnings("unchecked")
+		var clientes = (List<ClienteDTO>)model.get(ConstantesController.ATT_CLIENTE_LIST);
+		
+		var beanWriter = new CsvBeanWriter(response.getWriter(), CsvPreference.EXCEL_NORTH_EUROPE_PREFERENCE); 
+		
+		
+		
+		var header = new String[]{"id", "nombre", "apellido", "email", "createAt"};
+		beanWriter.writeHeader(header);
+	
+		for (var cl :  clientes) {
+			beanWriter.write(cl, header);
+		}
+		beanWriter.close();
+		
+	}
+
+
+	
+
+
+}
