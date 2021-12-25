1f37aca41dc29b66345a502afef8843224119aca
David de la Rosa Delgado <drd_id@hotmail.com>
2021-12-25 23:57:00 +0100

    251. View Technologies REST -> @ResponseBody

---
+++ spring-boot-data-jpa/src/main/java/com/drd/springbootpoc/app/controllers/rest/ClienteRestController.java
@@ -0,0 +1,29 @@
+package com.drd.springbootpoc.app.controllers.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.drd.springbootpoc.app.common.controller.AppController;
+import com.drd.springbootpoc.app.model.service.IClienteService;
+import com.drd.springbootpoc.app.view.xml.ClienteList;
+
+@RestController
+@RequestMapping("/api")
+public class ClienteRestController extends AppController{
+	
+	@Autowired
+	private IClienteService clienteService;
+	
+
+	@PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
+	@GetMapping(value={"/clientes/listar"})
+	public ClienteList listarRest() {
+		
+		return new ClienteList(clienteService.obtenerTodosClientes());
+	}	
+	
+
+}
