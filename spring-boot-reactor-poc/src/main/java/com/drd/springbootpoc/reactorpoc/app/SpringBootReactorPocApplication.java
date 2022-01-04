61a910e75019d93b453b4854fda0b718d806e63a
David de la Rosa Delgado <drd_id@hotmail.com>
2022-01-04 14:21:15 +0100

    365. Crear Flux a partir de Iterable

--- spring-boot-reactor-poc/src/main/java/com/drd/springbootpoc/reactorpoc/app/SpringBootReactorPocApplication.java
+++ spring-boot-reactor-poc/src/main/java/com/drd/springbootpoc/reactorpoc/app/SpringBootReactorPocApplication.java
@@ -1,5 +1,9 @@
 package com.drd.springbootpoc.reactorpoc.app;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -22,27 +26,35 @@ public class SpringBootReactorPocApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Andres Pelaez", "Pedro Martinez", "Maria Dorsal", 
-											"David the Best", "Rodrigo la Liaste", 
-											"Kevin Choni", "Pedro Fulano");
-		nombres.map( nombre -> new Usuario(
-						nombre.substring(0,nombre.indexOf(' ')).toUpperCase(),
-						nombre.substring(nombre.indexOf(' ')+1).toUpperCase()
-						))
-				.filter( usuario -> usuario.getNombre().equalsIgnoreCase("Pedro"))
-				.doOnNext(usuario-> {
-					if (usuario == null) {
-						throw new RuntimeException("Los usuarios no pueden ser nulos");
-					} else {
-						System.out.println("doOnNext -> " + usuario.toString());
-					}})
-				.map( usuario -> {
-					String nombre = usuario.getNombre().toLowerCase();
-					usuario.setNombre(nombre);
-					return usuario;
-					});
 		
-		nombres.subscribe(
+		
+		List<String> usuariosList = Arrays.asList("Andres Pelaez", "Pedro Martinez", "Maria Dorsal", 
+									"David the Best", "Rodrigo la Liaste", 
+									"Kevin Choni", "Pedro Fulano");
+		
+		Flux<String> usuarios = Flux.fromIterable(usuariosList);
+		
+//		Flux<String> nombres = Flux.just("Andres Pelaez", "Pedro Martinez", "Maria Dorsal", 
+//											"David the Best", "Rodrigo la Liaste", 
+//											"Kevin Choni", "Pedro Fulano");
+//		nombres.map( nombre -> new Usuario(
+//						nombre.substring(0,nombre.indexOf(' ')).toUpperCase(),
+//						nombre.substring(nombre.indexOf(' ')+1).toUpperCase()
+//						))
+//				.filter( usuario -> usuario.getNombre().equalsIgnoreCase("Pedro"))
+//				.doOnNext(usuario-> {
+//					if (usuario == null) {
+//						throw new RuntimeException("Los usuarios no pueden ser nulos");
+//					} else {
+//						System.out.println("doOnNext -> " + usuario.toString());
+//					}})
+//				.map( usuario -> {
+//					String nombre = usuario.getNombre().toLowerCase();
+//					usuario.setNombre(nombre);
+//					return usuario;
+//					});
+		
+		usuarios.subscribe(
 				log::info,
 				err -> log.error(String.format("Se ha producido un error: %s", err.toString())),
 				new Runnable() {
